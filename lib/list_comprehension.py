7beea5ac38eccf5049b443eb9f95df08be80ae7f
Daniel Morley <morleydan10@gmail.com>
2023-12-20 08:23:27 -0500

    solutions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    
+    exclamation = [(s + "!") for s in sentence_list if len(sentence_list) > 0 and isinstance(s, str)]
+    return exclamation
